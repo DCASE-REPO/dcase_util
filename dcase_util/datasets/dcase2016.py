35a34a6d5e19ca2c808d213d2d86e3f796376be5
Toni Heittola <toni.heittola@gmail.com>
2017-11-13 19:28:52 +0200

    Add CHiMEHome_DomesticAudioTag_EvaluationSet dataset

--- dcase_util/datasets/dcase2016.py
+++ dcase_util/datasets/dcase2016.py
@@ -295,3 +295,198 @@ class CHiMEHome_DomesticAudioTag_DevelopmentSet(AudioTaggingDataset):
 
         return self
 
+
+class CHiMEHome_DomesticAudioTag_EvaluationSet(CHiMEHome_DomesticAudioTag_DevelopmentSet):
+    def __init__(self,
+                 storage_name='CHiMeHome-audiotag-evaluation',
+                 data_path=None,
+                 included_content_types=None,
+                 sample_mode='16kHz',
+                 **kwargs):
+        """
+        Constructor
+
+        Parameters
+        ----------
+
+        storage_name : str
+            Name to be used when storing dataset on disk
+
+        data_path : str
+            Root path where the dataset is stored. If None, os.path.join(tempfile.gettempdir(), 'dcase_util_datasets')
+            is used.
+
+        included_content_types : list of str
+            Indicates what content type should be processed. One or multiple from ['all', 'audio', 'meta', 'code',
+            'documentation']. If None given, ['all'] is used.
+
+        sample_mode : str
+            Sample rate mode, '16kHz' or '48kHz'
+
+        """
+
+        kwargs['evaluation_setup_folder'] = 'evaluation_setup_evaluation'
+        kwargs['meta_filename'] = 'meta_evaluation.txt'
+        kwargs['local_path'] = os.path.join(data_path, 'CHiMeHome-audiotag-development')
+        kwargs['filelisthash_filename'] = 'filelist_evaluation.python.hash'
+
+        kwargs['included_content_types'] = included_content_types
+        kwargs['data_path'] = data_path
+        kwargs['storage_name'] = storage_name
+        kwargs['dataset_group'] = 'tag'
+        kwargs['dataset_meta'] = {
+            'authors': 'Peter Foster, Siddharth Sigtia, Sacha Krstulovic, Jon Barker, and Mark Plumbley',
+            'title': 'The CHiME-Home dataset is a collection of annotated domestic environment audio recordings',
+            'url': None,
+            'audio_source': 'Field recording',
+            'audio_type': 'Natural',
+            'recording_device_model': 'Unknown',
+            'microphone_model': 'Unknown',
+        }
+
+        kwargs['package_list'] = [
+            {
+                'content_type': ['documentation', 'meta', 'audio'],
+                'remote_file': 'https://archive.org/download/chime-home/chime_home.tar.gz',
+                'remote_bytes': 4162513717,
+                'remote_md5': '9ce9d584e229b465b3aa08db2d7fee67',
+                'filename': 'chime_home.tar.gz'
+            }
+        ]
+        kwargs['audio_paths'] = [
+            os.path.join('chime_home', 'chunks')
+        ]
+        super(CHiMEHome_DomesticAudioTag_EvaluationSet, self).__init__(**kwargs)
+        self.crossvalidation_folds = None
+        self.sample_mode = '.' + sample_mode
+        self.tag_map = {
+            'c': 'child speech',
+            'm': 'adult male speech',
+            'f': 'adult female speech',
+            'v': 'video game/tv',
+            'p': 'percussive sound',
+            'b': 'broadband noise',
+            'o': 'other',
+            'S': 'silence/background',
+            'U': 'unidentifiable'
+        }
+
+    def prepare(self):
+        """Prepare dataset for the usage.
+
+        Returns
+        -------
+        self
+
+        """
+        if not self.meta_container.exists():
+            scene_label = 'home'
+
+            evaluation_chunks = ListDictContainer(
+                filename=os.path.join(self.local_path, 'chime_home',
+                                      'evaluation_chunks_refined.csv')
+            ).load(fields=['id', 'filename', 'set_id'])
+
+            audio_files = {}
+            for item in dcase_cross_val_data:
+                audio_filename = os.path.join('chime_home', 'chunks', item['filename'] + self.sample_mode + '.wav')
+                annotation_filename = os.path.join('chime_home', 'chunks', item['filename'] + '.csv')
+
+                if audio_filename not in audio_files:
+                    audio_files[audio_filename] = {
+                        'audio': audio_filename,
+                        'meta': annotation_filename
+                    }
+
+            meta_data = MetaDataContainer()
+            for audio_filename, data in iteritems(audio_files):
+                current_meta_data = DictContainer(filename=os.path.join(self.local_path, data['meta'])).load()
+                tags = []
+                for i, tag in enumerate(current_meta_data['majorityvote']):
+                    if tag is not 'S' and tag is not 'U':
+                        tags.append(self.tagcode_to_taglabel(tag))
+
+                name = os.path.split(audio_filename)[1]
+                segment_name = name[0:name.find('_chunk')]
+                chunk_name = name[name.find('_chunk')+1:].split('.')[0]
+
+                item = MetaDataItem({
+                    'filename': audio_filename,
+                    'scene_label': scene_label,
+                    'tags': ';'.join(tags)+';',
+                    'identifier': segment_name
+                })
+
+                self.process_meta_item(item=item, absolute_path=False)
+
+                meta_data.append(item)
+
+            # Save meta
+            meta_data.save(filename=self.meta_file)
+
+            # Load meta and cross validation
+            self.load()
+
+        all_folds_found = True
+
+        train_filename = self.evaluation_setup_filename(
+            setup_part='train'
+        )
+
+        test_filename = self.evaluation_setup_filename(
+            setup_part='test'
+        )
+
+        eval_filename = self.evaluation_setup_filename(
+            setup_part='evaluate'
+        )
+
+        if not os.path.isfile(train_filename):
+            all_folds_found = False
+
+        if not os.path.isfile(test_filename):
+            all_folds_found = False
+
+        if not os.path.isfile(eval_filename):
+            all_folds_found = False
+
+        if not all_folds_found:
+            Path().makedirs(path=self.evaluation_setup_path)
+
+            # Train
+            train_filename = self.evaluation_setup_filename(
+                setup_part='train'
+            )
+
+            train_meta = MetaDataContainer(filename=train_filename)
+            for filename in self.train_files():
+                train_meta.append(self.file_meta(filename)[0])
+
+            train_meta.save()
+
+            # Test
+            test_filename = self.evaluation_setup_filename(
+                setup_part='test'
+            )
+
+            test_meta = MetaDataContainer(filename=test_filename)
+            for filename in self.test_files():
+                test_meta.append(MetaDataItem({'filename': self.absolute_to_relative(filename)}))
+
+            test_meta.save()
+
+            # Evaluate
+            eval_filename = self.evaluation_setup_filename(
+                setup_part='evaluate'
+            )
+
+            eval_meta = MetaDataContainer(filename=eval_filename)
+            for filename in self.test_files():
+                eval_meta.append(self.file_meta(filename)[0])
+
+            eval_meta.save()
+
+            # Load meta and cross validation
+            self.load()
+
+        return self
