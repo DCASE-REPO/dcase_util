c5afb684a5d55418e01056a5606d73240929aded
Toni Heittola <toni.heittola@gmail.com>
2018-01-29 16:04:29 +0200

    Update documentation

--- docs/tutorial_audio-1.py
+++
@@ -1,6 +0,0 @@
-import dcase_util
-audio_container = dcase_util.containers.AudioContainer().load(
-  filename=dcase_util.utils.Example.audio_filename()
-)
-audio_container.filename = None
-audio_container.plot_wave()
