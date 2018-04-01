56d07aa274726b27831e9fb1ca4d0d91bc679c28
toni_heittola <toni.heittola@gmail.com>
2018-04-02 00:19:42 +0300

    Unify axis naming

--- dcase_util/data/probabilities.py
+++ dcase_util/data/probabilities.py
@@ -14,7 +14,7 @@ class ProbabilityEncoder(ObjectContainer):
 
         Parameters
         ----------
-        label_list : list or str
+        label_list : list of str
             Label list
 
         """
@@ -23,16 +23,26 @@ class ProbabilityEncoder(ObjectContainer):
 
         self.label_list = label_list
 
-    def collapse_probabilities(self, probabilities, operator='sum', frame_axis=0):
-        """Collapse probabilities
+    def collapse_probabilities(self, probabilities, operator='sum', time_axis=1):
+        """Collapse probabilities along time_axis
 
         Parameters
         ----------
         probabilities : numpy.ndarray
-            Probabilities to be accumulated
+            Probabilities to be collapsed
 
         operator : str ('sum', 'prod', 'mean')
             Operator to be used
+            Default value 'sum'
+
+        time_axis : int
+            time axis
+            Default value 1
+
+        Raises
+        ------
+        AssertionError
+            Unknown operator
 
         Returns
         -------
@@ -50,30 +60,54 @@ class ProbabilityEncoder(ObjectContainer):
             self.logger.exception(message)
             raise AssertionError(message)
 
-        accumulated = numpy.ones(probabilities.shape[frame_axis]) * -numpy.inf
-        for row_id in range(0, probabilities.shape[frame_axis]):
+        # Get data_axis
+        if time_axis == 0:
+            data_axis = 1
+        else:
+            data_axis = 0
+
+        # Initialize array to store results
+        accumulated = numpy.ones(probabilities.shape[data_axis]) * -numpy.inf
+
+        # Loop along data_axis
+        for class_id in range(0, probabilities.shape[data_axis]):
+            # Get current array
+            if time_axis == 0:
+                current_array = probabilities[:, class_id]
+
+            elif time_axis == 1:
+                current_array = probabilities[class_id, :]
+
+            # Collapse array with given operator
             if operator == 'sum':
-                accumulated[row_id] = numpy.sum(probabilities[row_id, :])
+                accumulated[class_id] = numpy.sum(current_array)
+
             elif operator == 'prod':
-                accumulated[row_id] = numpy.prod(probabilities[row_id, :])
+                accumulated[class_id] = numpy.prod(current_array)
+
             elif operator == 'mean':
-                accumulated[row_id] = numpy.mean(probabilities[row_id, :])
+                accumulated[class_id] = numpy.mean(current_array)
 
         return accumulated
 
-    def collapse_probabilities_windowed(self, probabilities, window_length, operator='sliding_sum'):
-        """Collapse probabilities in windows
+    def collapse_probabilities_windowed(self, probabilities, window_length, operator='sliding_sum', time_axis=1):
+        """Collapse probabilities with a sliding window. Window hop size is one.
 
         Parameters
         ----------
         probabilities : numpy.ndarray
-            Probabilities to be accumulated
+            Probabilities to be collapsed
 
         window_length : int
-            Window length in analysis frame amount
+            Window length in analysis frame amount.
 
         operator : str ('sliding_sum', 'sliding_mean', 'sliding_median')
             Operator to be used
+            Default value 'sliding_sum'
+
+        time_axis : int
+            time axis
+            Default value 1
 
         Raises
         ------
@@ -96,30 +130,54 @@ class ProbabilityEncoder(ObjectContainer):
             self.logger.exception(message)
             raise AssertionError(message)
 
+        # Get data_axis
+        if time_axis == 0:
+            data_axis = 1
+        else:
+            data_axis = 0
+
         # Lets keep the system causal and use look-back while smoothing (accumulating) likelihoods
         output_probabilities = copy.deepcopy(probabilities)
-        for stop_id in range(0, probabilities.shape[0]):
-            start_id = stop_id - window_length
 
-            if start_id < 0:
-                start_id = 0
+        # Loop along data_axis
+        for class_id in range(0, probabilities.shape[data_axis]):
 
-            if start_id != stop_id:
-                if operator == 'sliding_sum':
-                    output_probabilities[start_id] = numpy.sum(probabilities[start_id:stop_id])
+            # Get current array
+            if time_axis == 0:
+                current_array = probabilities[:, class_id]
 
-                elif operator == 'sliding_mean':
-                    output_probabilities[start_id] = numpy.mean(probabilities[start_id:stop_id])
+            elif time_axis == 1:
+                current_array = probabilities[class_id, :]
 
-                elif operator == 'sliding_median':
-                    output_probabilities[start_id] = numpy.median(probabilities[start_id:stop_id])
+            # Loop windows
+            for stop_id in range(0, probabilities.shape[time_axis]):
+                start_id = stop_id - window_length
 
-            else:
-                output_probabilities[start_id] = probabilities[start_id]
+                if start_id < 0:
+                    start_id = 0
+
+                if start_id != stop_id:
+                    if operator == 'sliding_sum':
+                        current_result = numpy.sum(current_array[start_id:stop_id])
+
+                    elif operator == 'sliding_mean':
+                        current_result = numpy.mean(current_array[start_id:stop_id])
+
+                    elif operator == 'sliding_median':
+                        current_result = numpy.median(current_array[start_id:stop_id])
+
+                else:
+                    current_result = current_array[start_id]
+
+                if time_axis == 0:
+                    output_probabilities[start_id, class_id] = current_result
+
+                elif time_axis == 1:
+                    output_probabilities[class_id, start_id] = current_result
 
         return output_probabilities
 
-    def binarization(self, probabilities, binarization_type='global_threshold', threshold=0.5, frame_axis=0):
+    def binarization(self, probabilities, binarization_type='global_threshold', threshold=0.5, time_axis=1):
         """Binarization
 
         Parameters
@@ -131,9 +189,11 @@ class ProbabilityEncoder(ObjectContainer):
 
         threshold : float
             Binarization threshold, value of the threshold are replaced with 1 and under with 0.
+            Default value 0.5
 
-        frame_axis : int
+        time_axis : int
             Axis index for the frames
+            Default value 1
 
         Raises
         ------
@@ -156,32 +216,62 @@ class ProbabilityEncoder(ObjectContainer):
             self.logger.exception(message)
             raise AssertionError(message)
 
+        # Get data_axis
+        if time_axis == 0:
+            data_axis = 1
+        else:
+            data_axis = 0
+
         if binarization_type == 'global_threshold':
             return numpy.array(probabilities >= threshold, dtype=int)
 
         elif binarization_type == 'class_threshold' and isinstance(threshold, list):
             data = []
             for class_id, class_threshold in enumerate(threshold):
-                if frame_axis == 0:
+                if data_axis == 0:
                     data.append(numpy.array(probabilities[class_id, :] >= class_threshold, dtype=int))
-                elif frame_axis == 1:
+
+                elif data_axis == 1:
                     data.append(numpy.array(probabilities[:, class_id] >= class_threshold, dtype=int))
 
-            if frame_axis == 0:
+            if data_axis == 0:
                 return numpy.vstack(data)
-            elif frame_axis == 1:
+
+            elif data_axis == 1:
                 return numpy.vstack(data).T
 
         elif binarization_type == 'frame_max':
-            if frame_axis == 0:
+            if data_axis == 0:
                 return numpy.array((probabilities / numpy.max(probabilities, axis=0)) == 1, dtype=int)
 
-            elif frame_axis == 1:
+            elif data_axis == 1:
                 return numpy.array((probabilities.T / numpy.max(probabilities, axis=1)).T == 1, dtype=int)
 
-    def max_selection(self, probabilities):
+    def max_selection(self, probabilities, label_list=None):
+        """Selection based on maximum probability
+
+        Parameters
+        ----------
+        probabilities : numpy.ndarray
+            Probabilities
+
+        label_list : list of str
+            Label list
+            Default value None
+
+        Returns
+        -------
+        numpy.ndarray
+
+        """
+
+        if label_list is None:
+            label_list = self.label_list
+
         class_id = numpy.argmax(probabilities)
-        if class_id < len(self.label_list):
-            return self.label_list[class_id]
+
+        if class_id < len(label_list):
+            return label_list[class_id]
+
         else:
             return None
