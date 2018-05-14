937cba266bfdb05aecdb493d940c73f829164c64
toni_heittola <toni.heittola@gmail.com>
2018-05-14 22:58:45 +0300

    Update template, Fix markdown file modification detection

--- dcase_util/tools/datasets.py
+++ dcase_util/tools/datasets.py
@@ -49,54 +49,55 @@ class DatasetPacker(object):
         self.package_extension = package_extension
 
         if md_to_html_template is None:
-            self.md_to_html_template = """<!DOCTYPE html>
-                <html>
-                <head>
-                    <title>{{title}}</title>
-                     <meta charset="UTF-8">
-                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+            self.md_to_html_template = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{title}}</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
-                    <link rel="stylesheet" href="https://cdn.rawgit.com/afeld/bootstrap-toc/v1.0.0/dist/bootstrap-toc.min.css"> 
-                    <script src="https://cdn.rawgit.com/afeld/bootstrap-toc/v1.0.0/dist/bootstrap-toc.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/afeld/bootstrap-toc/v1.0.0/dist/bootstrap-toc.min.css"> 
+    <script src="https://cdn.rawgit.com/afeld/bootstrap-toc/v1.0.0/dist/bootstrap-toc.min.js"></script>
 
-                    <style>        
-                        p {
-                            text-align: justify;
-                        }
-                        nav[data-toggle='toc'] {
-                            top: 42px;
-                        }
-                        @media (max-width: 768px) {
-                          nav[data-toggle='toc'] {
-                            margin-bottom: 42px;
-                            position: static;
-                          }
-                          nav[data-toggle='toc'] .nav .nav {
-                            display: block;
-                          }
-                        }
-                        pre {
-                            padding: 1.5rem;
-                            background-color: #f8f9fa;
-                        }
-                    </style>
-                </head>
-                <body data-spy="scroll" data-target="#toc">
-                    <div class="container">
-                        <div class="row">                            
-                            <div class="col-sm-3">
-                                <nav id="toc" data-toggle="toc" class="sticky-top"></nav>
-                            </div>
-                            <div class="col-sm-9">
-                                {{content}}
-                            </div>
-                        </div>
-                    </div>
-                </body>
-                </html>
+    <style>        
+        p {
+            text-align: justify;
+        }
+        nav[data-toggle='toc'] {
+            top: 42px;
+        }
+        @media (max-width: 768px) {
+          nav[data-toggle='toc'] {
+            margin-bottom: 42px;
+            position: static;
+          }
+          nav[data-toggle='toc'] .nav .nav {
+            display: block;
+          }
+        }
+        pre {
+            padding: 1.5rem;
+            background-color: #f8f9fa;
+        }
+    </style>
+</head>
+<body data-spy="scroll" data-target="#toc">
+    <div class="container">
+        <div class="row">                            
+            <div class="col-sm-3">
+                <nav id="toc" data-toggle="toc" class="sticky-top"></nav>
+            </div>
+            <div class="col-sm-9">
+                {{content}}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
                 """
         else:
             self.md_to_html_template = md_to_html_template
@@ -206,9 +207,7 @@ class DatasetPacker(object):
                     new_files = []
                     for item in group['file_list']:
                         if os.path.splitext(item['source'])[-1] == '.md':
-                            if not os.path.exists(os.path.splitext(item['source'])[0] + '.html') or (
-                                    os.path.exists(os.path.splitext(item['source'])[0] + '.html') and os.path.getmtime(
-                                    item['source']) > os.path.splitext(item['source'])[0] + '.html') or overwrite:
+                            if not os.path.exists(os.path.splitext(item['source'])[0] + '.html') or (os.path.exists(os.path.splitext(item['source'])[0] + '.html') and os.path.getmtime(item['source']) > os.path.getmtime(os.path.splitext(item['source'])[0] + '.html')) or overwrite:
 
                                 # Convert
                                 self.convert_markdown(
