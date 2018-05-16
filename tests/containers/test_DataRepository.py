2704c4c6aea66f76352c3b1806475a17eb807446
toni_heittola <toni.heittola@gmail.com>
2018-05-16 22:06:29 +0300

    Add unit tests DataRepository

--- tests/containers/test_DataRepository.py
+++ tests/containers/test_DataRepository.py
@@ -39,6 +39,43 @@ def test_container():
     nose.tools.eq_(data_repository.get_container(label='label1', stream_id='stream0'), {'data': 123})
 
 
+def test_save_load():
+    data_repository = dcase_util.containers.DataRepository(
+        data={
+            'label1': {
+                'stream0': {
+                    'data': 100
+                },
+                'stream1': {
+                    'data': 200
+                }
+            },
+            'label2': {
+                'stream0': {
+                    'data': 300
+                },
+                'stream1': {
+                    'data': 400
+                }
+            }
+        }
+    )
+
+    # PICKLE
+    tmp = tempfile.NamedTemporaryFile('r+', suffix='.cpickle',  dir='/tmp', delete=False)
+    try:
+        data_repository.save(filename=tmp.name)
+        data_repository_loaded = dcase_util.containers.DataRepository().load(filename=tmp.name)
+        nose.tools.eq_(data_repository_loaded.labels, ['label1', 'label2'])
+        nose.tools.eq_(data_repository_loaded.stream_ids('label1'), ['stream0', 'stream1'])
+        nose.tools.eq_(data_repository_loaded.stream_ids('label2'), ['stream0', 'stream1'])
+
+        nose.tools.eq_(data_repository_loaded.get_container(label='label1', stream_id='stream0'), {'data': 100})
+
+    finally:
+        os.unlink(tmp.name)
+
+
 def test_log():
     with dcase_util.utils.DisableLogger():
         data_repository = dcase_util.containers.DataRepository(
