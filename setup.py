3d4ae742ea203a2b7e798ea7065614fc094977ef
toni_heittola <toni.heittola@gmail.com>
2018-05-16 10:49:00 +0300

    Increase version number

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except (IOError, ImportError, RuntimeError):
 
 setup(
     name='dcase_util',
-    version='0.2.1',
+    version='0.2.2',
     description='A collection of utilities for Detection and Classification of Acoustic Scenes and Events',
     author='Toni Heittola',
     author_email='toni.heittola@gmail.com',
